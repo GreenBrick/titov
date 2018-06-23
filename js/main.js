b050afafcd2bf170628ae15d891cc5674db71599
Alex Titov <brick029@gmail.com>
2018-06-23 21:50:49 +0300

    Изменение футера

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
 
-	$('.start').click(function(){
-        $('.bottom__car').animate({'left':'3000'}, 5000, function(){
-	        $('.end').html('<p>Вот теперь точно, конец...</p>');
-	    });
-    });
-
 });
